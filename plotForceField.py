1d485f90fc82555badefbea4fc75255f1aab03cf
Arnaud Mercier <38730912+armercier@users.noreply.github.com>
2018-11-09 09:28:06 -0500

    starting to implement the scalar field display function

--- plotForceField.py
+++ plotForceField.py
@@ -1,8 +1,11 @@
 import numpy as np
 from mpl_toolkits.mplot3d import axes3d
+from mpl_toolkits.mplot3d import Axes3D
 import matplotlib
 matplotlib.use("TkAgg")
 from matplotlib import pyplot as plt
+from matplotlib import cm
+
 
 class displaySimualtion:
 
@@ -24,7 +27,17 @@ class displaySimualtion:
         ax.quiver(dataDict['x'], dataDict['y'], dataDict['z'], dataDict['Fx'], dataDict['Fy'], dataDict['Fz'], length=0.1, normalize=True)
         plt.show()
 
+    def displayABSPressionField(self,dataDict):
 
+        # x = np.linspace(-3, 3, 256)
+        # y = np.linspace(-3, 3, 256)
+        # X, Y = np.meshgrid(x, y)
+        # Z = np.sinc(np.sqrt(X ** 2 + Y ** 2))
+
+        fig = plt.figure()
+        ax = fig.gca(projection = '3d')
+        ax.plot_surface(X, Y, Z, cmap=cm.gray)
+        plt.show()
 
 
 d = displaySimualtion()
