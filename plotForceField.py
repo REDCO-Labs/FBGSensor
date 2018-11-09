3de94ceb9a0853464d998baca13d835e9f1bbb19
Arnaud Mercier <38730912+armercier@users.noreply.github.com>
2018-11-09 09:16:35 -0500

    Update plotForceField.py
    3D vector plot added

--- plotForceField.py
+++ plotForceField.py
@@ -1,4 +1,32 @@
 import numpy as np
+from mpl_toolkits.mplot3d import axes3d
+import matplotlib
+matplotlib.use("TkAgg")
+from matplotlib import pyplot as plt
 
-x, y, z, absP, Fx, Fy, Fz = np.loadtxt('Pression-et-Force-XYZ.txt',unpack=True,skiprows=2,delimiter=',')
+class displaySimualtion:
+
+    def __init__(self):
+        self.sauce = 1
+
+    def showSimulation(self):
+        filename = 'Pression-et-Force-XYZ.txt'
+        dataDict = self.importSimulationData(filename)
+        self.displayVectorField(dataDict)
+    def importSimulationData(self, filename):
+        x, y, z, absP, Fx, Fy, Fz = np.loadtxt('{}'.format(filename),unpack=True,skiprows=2,delimiter=',')
+        dataDict = {'x':x,'y':y,'z':z,'Fx':Fx,'Fy':Fy,'Fz':Fz, 'absP':absP}
+        return dataDict
+
+    def displayVectorField(self,dataDict):
+        fig = plt.figure()
+        ax = fig.gca(projection='3d')
+        ax.quiver(dataDict['x'], dataDict['y'], dataDict['z'], dataDict['Fx'], dataDict['Fy'], dataDict['Fz'], length=0.1, normalize=True)
+        plt.show()
+
+
+
+
+d = displaySimualtion()
+d.showSimulation()
 
